3fedfbb8d65fb8d9e767111263cd98e327f5b8bd
Munjal <munjalblockchain@gmail.com>
2020-02-02 20:51:21 +0530

    orderManagement with CAs & CouchDB

---
+++ new.js
@@ -0,0 +1,6 @@
+How to call rich queries
+    StatusBuilder.getQueryResult(queryString)
+
+CouchDB query: peer chaincode query -C amchannel -c ("Args": ["query", "{\"docType\": \"order\"}"}}'
+'
+querystring= 
